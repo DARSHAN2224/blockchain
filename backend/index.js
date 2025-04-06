b0d30befd15bb6062c848bfa9c8a1c6395a784f5
darshan <pdarshan2224@gmail.com>
2025-04-06 18:59:07 +0530

    fourth

--- backend/index.js
+++ backend/index.js
@@ -4,26 +4,29 @@ import dotenv from 'dotenv';
 import cors  from 'cors';
 import path  from 'path';
 import router from './routers/mainRoute.js';
+
 const app = express();
 dotenv.config();
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(express.json())
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT=process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+
 app.use("/v1", router);
 
+// ✅ Only serve frontend in production mode
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get(/.*/, (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });  
 }
-// app.use("/");
 
-app.listen(PORT,()=>{
-    connectDb()
-    console.log("Server started on port:",PORT);
+app.listen(PORT, () => {
+  connectDb();
+  console.log("Server started on port:", PORT);
 });
-
