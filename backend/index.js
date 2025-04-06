c356c3553413ea40ef2c8850bbf33514a15ec428
darshan <pdarshan2224@gmail.com>
2025-04-06 18:20:17 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,16 @@ app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 const PORT=process.env.PORT || 5000;
-// const __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+const __dirname = path.resolve();
 app.use("/v1", router);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 // app.use("/");
 
 app.listen(PORT,()=>{
